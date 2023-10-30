70098aeaea9718cfc302616474c8d72a7236ffc0
Rohit Ranjan <136880256+Tech-itRohit@users.noreply.github.com>
2023-10-30 10:43:07 +0530

    Update CustomWebAuthenticationDetails

--- project-login/src/main/java/com/log/jwt/CustomWebAuthenticationDetails.java
+++ project-login/src/main/java/com/log/jwt/CustomWebAuthenticationDetails.java
@@ -1,17 +1,17 @@
 package com.log.jwt;
-import javax.servlet.http.HttpServletRequest;
+// import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
-public class CustomWebAuthenticationDetails extends WebAuthenticationDetails {
-    private final String token;
+// public class CustomWebAuthenticationDetails extends WebAuthenticationDetails {
+//     private final String token;
 
-    public CustomWebAuthenticationDetails(HttpServletRequest request) {
-        super(request);
-        this.token = request.getHeader("Authorization");
-    }
+//     public CustomWebAuthenticationDetails(HttpServletRequest request) {
+//         super(request);
+//         this.token = request.getHeader("Authorization");
+//     }
 
-    public String getToken() {
-        return token;
-    }
+//     public String getToken() {
+//         return token;
+//     }
 }
